5ecd1b61eebe96dad019862db12d2b4f82e20053
Anshdeep Bansal <udaibansal123@gmail.com>
2025-02-20 21:26:00 +0530

    Initial commit - WPF To-Do List

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace ToDoAppWPF
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<TaskItem> Tasks { get; set; } = new ObservableCollection<TaskItem>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            taskListBox.ItemsSource = Tasks;
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                Tasks.Add(new TaskItem { Name = txtTask.Text, IsCompleted = false });
+                txtTask.Clear();
+            }
+        }
+
+        private void RemoveTask_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedTask = taskListBox.SelectedItem as TaskItem;
+            if (selectedTask != null)
+            {
+                Tasks.Remove(selectedTask);
+            }
+        }
+    }
+
+    public class TaskItem : INotifyPropertyChanged
+    {
+        private bool _isCompleted;
+        public string Name { get; set; }
+
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set
+            {
+                _isCompleted = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsCompleted)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
